133942b8fb69250343752a6999cc00de46b0fc9c
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-14 18:39:35 -0500

    Added a no-default option.

--- src/options.js
+++ src/options.js
@@ -4,6 +4,7 @@ export const copyOptions = (target, defaults, ...sources) => {
   target = target || {};
   const keys = Object.keys(defaults);
   for (const key of keys) {
+    if (defaults[key] === undefined) continue;
     target[key] = defaults[key];
   }
   for (const source of sources) {
