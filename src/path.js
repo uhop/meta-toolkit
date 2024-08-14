ae41e0a8ce3ad9258db12d2fee9ae9d89a11496f
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-14 18:02:57 -0500

    More path functions.

--- src/path.js
+++ src/path.js
@@ -2,26 +2,61 @@
 
 const dereferable = {object: 1, function: 1};
 
-export const get = (obj, path, delimiter = '.', defaultValue) => {
+export const get = (object, path, delimiter = '.', defaultValue) => {
   if (typeof path == 'string') {
     path = path.split(delimiter);
   }
   for (let i = 0; i < path.length; ++i) {
-    if (!obj || !dereferable[typeof obj]) return defaultValue;
-    obj = obj[path[i]];
+    if (!object || !dereferable[typeof object]) return defaultValue;
+    object = object[path[i]];
   }
-  return obj;
+  return object;
 };
 
-export const set = (obj, path, value, delimiter = '.', defaultValue) => {
+export const set = (object, path, value, delimiter = '.', defaultValue) => {
+  if (typeof path == 'string') {
+    path = path.split(delimiter);
+  }
+  let parent = null;
+  for (let i = 0; i < path.length; ++i) {
+    if (!object || !dereferable[typeof object]) return defaultValue;
+    parent = object;
+    object = object[path[i]];
+  }
+  if (!parent) return defaultValue;
+  parent[path[path.length - 1]] = value;
+  return object;
+};
+
+export const forceSet = (object, path, value, delimiter = '.') => {
+  if (!object || !dereferable[typeof object]) throw new TypeError('Invalid object');
+  if (typeof path == 'string') {
+    path = path.split(delimiter);
+  }
+  let parent = null;
+  for (let i = 0; i < path.length; ++i) {
+    if (!object || !dereferable[typeof object]) {
+      object =parent[path[i - 1]] = {};
+    }
+    parent = object;
+    object = object[path[i]];
+  }
+  if (!parent) throw new TypeError('Empty path');
+  parent[path[path.length - 1]] = value;
+  return object;
+};
+
+export const remove = (object, path, delimiter = '.', defaultValue) => {
   if (typeof path == 'string') {
     path = path.split(delimiter);
   }
   if (!path.length) return defaultValue;
-  for (let i = 0; i < path.length - 1; ++i) {
-    if (!obj || !dereferable[typeof obj]) return defaultValue;
-    obj = obj[path[i]];
+  let parent = null;
+  for (let i = 0; i < path.length; ++i) {
+    if (!object || !dereferable[typeof object]) return defaultValue;
+    parent = object;
+    object = object[path[i]];
   }
-  if (!obj || !dereferable[typeof obj]) return defaultValue;
-  return (obj[path[path.length - 1]] = value);
+  delete parent[path[path.length - 1]];
+  return object;
 };
