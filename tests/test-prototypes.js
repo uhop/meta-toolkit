56bd53c5f2db2a431b7ae34202d0d2974a4324aa
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-14 21:51:34 -0500

    Added tests for prototypes.

---
+++ tests/test-prototypes.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import test from 'tape-six';
+
+import {prototypes, getPropertyDescriptor} from 'meta-toolkit/prototypes.js';
+
+class Foo {
+  foo() {}
+}
+
+class Bar extends Foo {
+  bar() {}
+}
+
+class Baz extends Bar {
+  baz() {}
+}
+
+test('Prototypes: prototypes()', t => {
+  const baz = new Baz();
+
+  t.deepEqual([...prototypes(baz)], [baz, Baz.prototype, Bar.prototype, Foo.prototype]);
+  t.deepEqual([...prototypes(baz)].map(p => p.constructor.name), ['Baz', 'Baz', 'Bar', 'Foo']);
+
+  t.deepEqual([...prototypes(baz, true)], [Baz.prototype, Bar.prototype, Foo.prototype]);
+  t.deepEqual([...prototypes(baz, true)].map(p => p.constructor.name), ['Baz', 'Bar', 'Foo']);
+});
+
+test('Prototypes: getPropertyDescriptor()', t => {
+  const baz = new Baz();
+
+  t.equal(getPropertyDescriptor(baz, 'foo')?.value?.name, 'foo');
+  t.equal(getPropertyDescriptor(baz, 'bar')?.value?.name, 'bar');
+  t.equal(getPropertyDescriptor(baz, 'baz')?.value?.name, 'baz');
+
+  t.notOk(getPropertyDescriptor(baz, 'quux'));
+});
