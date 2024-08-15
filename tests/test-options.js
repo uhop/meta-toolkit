2fc2655953ea77a41facaa1ce7bf7e900ec21eea
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-14 22:22:15 -0500

    Added tests for options.

---
+++ tests/test-options.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import test from 'tape-six';
+
+import {copyOptions} from 'meta-toolkit/options.js';
+
+test('Options: copyOptions()', t => {
+  const object = {foo: 1, bar: 2, baz: 3};
+
+  copyOptions(object, {foo: 10}, {foo: 100, bar: 200});
+  t.deepEqual(object, {foo: 100, bar: 2, baz: 3});
+
+  copyOptions(object, {foo: 10}, {bar: 200, baz: 300});
+  t.deepEqual(object, {foo: 10, bar: 2, baz: 3});
+
+  copyOptions(object, {foo: 10, quux: void 0}, {foo: 1, baz: 300});
+  t.deepEqual(object, {foo: 1, bar: 2, baz: 3});
+
+  copyOptions(object, {foo: 10, quux: void 0}, {quux: 4});
+  t.deepEqual(object, {foo: 10, bar: 2, baz: 3, quux: 4});
+
+  copyOptions(object, {foo: 10, bar: 20}, {foo: 100}, {bar: 200});
+  t.deepEqual(object, {foo: 100, bar: 200, baz: 3, quux: 4});
+
+  const newObject = copyOptions({}, {foo: 0, bar: 0}, object);
+  t.deepEqual(newObject, {foo: 100, bar: 200});
+});
